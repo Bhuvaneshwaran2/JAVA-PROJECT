da24f6f933084ad349149a0c2e2ff7ec2602727d
Bhuvaneshwaran2 <iambnk20@gmail.com>
2026-07-08 19:20:11 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Number of test cases
+        int t = 10;
+
+        while (t-- > 0) {
+
+            // Read input
+            int n = 10;
+            System.out.println("Enter the ");
+
+
+        }
+
+        sc.close();
+    }
+}
